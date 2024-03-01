2aceec607d17e50a139ae04251013b126432f47b
Jhonny <jagomezv@ufpso.edu.co>
2024-02-29 21:21:28 -0500

    hola mundo con spring

---
+++ src/test/java/com/processes/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processes.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
