0943f139013921c016ca72f6099e7c57fb77d85c
Jhonny A <jagomezv@ufpso.edu.co>
2024-03-21 21:21:10 -0500

    se creo las tablas de bd

--- src/main/java/com/processes/shop/model/User.java
+++ src/main/java/com/processes/shop/model/User.java
@@ -1,20 +1,29 @@
 package com.processes.shop.model;
 import com.processes.shop.model.enums.DocumentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
     private String fullName;
-    private Date birthday;
+    private LocalDate birthday;
     private DocumentType documentType;
-    private String document;
     private String phoneNumber;
     private String email;
     private String password;
-    private List<Address> address;
+    //private List<Address> address;
 }
--- src/main/java/com/processes/shop/service/UserServiceImpl.java
+++ src/main/java/com/processes/shop/service/UserServiceImpl.java
@@ -3,49 +3,49 @@ package com.processes.shop.service;
 import com.processes.shop.model.Address;
 import com.processes.shop.model.User;
 import com.processes.shop.model.enums.DocumentType;
+import com.processes.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User createUser(User user) {
-        return null;
+
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(User userUpdated, Long id) {
-        return null;
+
+        Optional<User> userBd = userRepository.findById(id);
+        if (userBd.isEmpty()){
+            return null;
+        }
+        userBd.get().setFullName(userUpdated.getFullName());
+        userBd.get().setPhoneNumber(userUpdated.getPhoneNumber());
+        return userRepository.save(userBd.get());
     }
 
     @Override
     public User getUserById(Long id) {
-        return User.builder()
-                .fullName("Pepito perez")
-                .document("123")
-                .documentType(DocumentType.CC)
-                .address(
-                        List.of(
-                            Address.builder()
-                                .avenue("1212")
-                                .neighborhood("12131")
-                                .build(),
-
-                                Address.builder()
-                                        .avenue("123")
-                                        .neighborhood("321")
-                                        .build()
-
-                        )
-
-                )
-                .build();
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()){
+            return null;
+        }
+        return user.get();
     }
 
     @Override
     public List<User> findAllUsers() {
-        return null;
+
+        return (List<User>) userRepository.findAll();
     }
 }
